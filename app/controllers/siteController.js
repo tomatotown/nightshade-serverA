2ee4f2a218a01b6b8e93a14474306a166e177ba5
Chang Xin <xchang.chn@gmail.com>
2015-05-06 09:54:43 +0800

    removed console.log

--- app/controllers/siteController.js
+++ app/controllers/siteController.js
@@ -9,8 +9,6 @@ router.get('/', function(req, res) {
 router.post('/login', function(req, res) {
     var username = req.body.username;
     var password = req.body.password;
-    console.log(username);
-    console.log(password);
     user.find({username: username, password: password}, function (err, user) {
         if (!err) {
             res.json({"username": user[0].username, "password": user[0].password});
